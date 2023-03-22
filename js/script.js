21d5b5aea6d5eac3d8038f4d585deb3425bc145f
maricamola <maricamola98@libero.it>
2023-03-22 11:25:18 +0100

    queryselector remove(hide) added background modified

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ const name = document.getElementById('name');
 const kmDaPercorrere = document.getElementById('kmDaPercorrere');
 const etaPasseggero = document.getElementById('etaPasseggero');
 const costoChilometro = 0.21;
+const carrozza = Math.ceil(Math.random()*12);
+const codiceCp = Math.floor(Math.random() * (99000 - 10000 +1) + 10000);
+
+outputcarrozza = document.getElementById('outputcarrozza');
+outputcodicecp = document.getElementById('outputcodicecp');
 
 // valori che potrebbero cambiare => t1
 
@@ -16,16 +21,17 @@ document.querySelector('#genera').addEventListener('click', function() {
     PrezzoBiglietto = PrezzoBiglietto * 0.6;
   }
 
-  document.getElementById('output').innerHTML = `Il costo del biglietto è ${PrezzoBiglietto.toFixed(2)} €`;
-});
+  document.getElementById('output').innerHTML = ` 
+  ${PrezzoBiglietto.toFixed(2)} €`;
 
+  document.querySelector('.ticket').classList.remove('hide');
+});
 
 document.querySelector('#reset').addEventListener('click', function(){
   name.value = "";
   kmDaPercorrere.value = "";
   document.querySelectorAll('input').reset();
 });
-
 
 
 
